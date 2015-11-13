f4756bcbd91e7d9bbf59417b90378d8956203d86
ZSShen <andy780328@hotmail.com>
2015-11-14 01:49:49 +0800

    More application for calling c from assembly.

--- AssemblyProgramming/android_x86/jni/cpp_site.cc
+++ AssemblyProgramming/android_x86/jni/cpp_site.cc
@@ -1,14 +1,24 @@
 #include <iostream>
-#include <asm_site.h>
+#include <bridge.h>
 
 
 int main()
 {
     Table tbl;
     AsmFunction(&tbl);
-    std::cout << tbl.m_First << std::endl;
-    std::cout << tbl.m_Second << std::endl;
-    std::cout << tbl.m_Third << std::endl;
-
+    std::cout << "[+] In main " << tbl.m_First << std::endl;
+    std::cout << "[+] In main " << tbl.m_Second << std::endl;
+    std::cout << "[+] In main " << tbl.m_Third << std::endl;
+    std::cout << "[+] In main " << tbl.m_Forth << std::endl;
     return 0;
+}
+
+
+int CFunction(int op1, int op2, int op3)
+{
+    int res = op1 + op2 + op3;
+    std::cout << "[+] In C site " << op1 << std::endl;
+    std::cout << "[+] In C site " << op2 << std::endl;
+    std::cout << "[+] In C site " << op3 << std::endl;
+    return res;
 }
