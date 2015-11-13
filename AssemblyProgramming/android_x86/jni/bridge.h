38a9d18ddf967c5f1be7cb76df0243752ea54294
ZSShen <andy780328@hotmail.com>
2015-11-14 01:54:09 +0800

    More application for calling c from assembly.

---
+++ AssemblyProgramming/android_x86/jni/bridge.h
@@ -0,0 +1,17 @@
+#ifndef _BRIDGE_H_
+#define _BRIDGE_H_
+
+
+struct Table
+{
+    int m_First;
+    int m_Second;
+    int m_Third;
+    int m_Forth;
+};
+
+extern void AsmFunction(Table*) __asm__("AsmFunction");
+extern "C" int CFunction(int, int, int);
+
+
+#endif
